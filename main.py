6aef64ce27ae0b8d6da9d9d8818a2eccf60cd230
Abdullah Al Faroque <afaroque@csafeglobal.com>
2024-10-02 22:52:17 +0600

    added dflix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, session, redirect, url_for, flash
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -6,14 +6,17 @@ import os
 from io import BytesIO
 
 app = Flask(__name__)
-
+app.secret_key = os.urandom(24)
 
 # Function to fetch valid links
 def fetch_valid_links_from_table(url):
+    # Create a session to maintain login state
+    s = requests.Session()
+    if 'login_session' in session:
+        s.cookies.update(session['login_session'])
     try:
         # Fetch the content of the URL
-        response = requests.get(url)
-        print(response.text)
+        response = s.get(url)
         response.raise_for_status()  # Raise an error if the request failed
     except requests.exceptions.RequestException as e:
         print(f"Error fetching the URL: {e}")
@@ -43,7 +46,36 @@ def fetch_valid_links_from_table(url):
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
 
+    # Create a session to maintain login state
+    s = requests.Session()
+    response = s.post('https://dflix.discoveryftp.net/login/auth', data={
+        'username': username,
+        'password': password,
+        'remember': 'on',
+        'loginsubmit': ''
+    })
+
+    if response.ok:
+        # Parse the response to check for error messages
+        soup = BeautifulSoup(response.text, 'html.parser')
+        error_message = soup.find('h6', class_='text-danger text-center')
+
+        if error_message:  # Check if there is an error message indicating login failure
+            flash('Login failed: ' + error_message.text.strip(), 'error')
+            return redirect(url_for('index'))  # Redirect back to index with error message
+
+        # If there's no error, login is successful
+        session['login_session'] = s.cookies.get_dict()  # Store session cookies
+        flash('Login successful!', 'success')
+        return redirect(url_for('index'))  # Redirect to home after successful login
+    else:
+        flash('Login failed: Unknown error', 'error')  # Handle unexpected errors
+        return redirect(url_for('index'))
 @app.route('/fetch-links', methods=['GET', 'POST'])
 def fetch_links():
     if request.method == 'POST':
